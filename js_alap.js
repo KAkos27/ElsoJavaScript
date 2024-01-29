ae298d656e941e4f22ea0e0f542b072f5002f8b8
KAkos27 <akoskosztolanyi@gmail.com>
2024-01-29 12:10:24 +0100

    onallo_resz

--- js_alap.js
+++ js_alap.js
@@ -7,6 +7,7 @@ function feltolt() {
     veletlenTomb[i] = vel;
   }
 }
+
 function kiir() {
   var szoveg = "";
   for (var i = 0; i < 50; i++) {
@@ -16,4 +17,46 @@ function kiir() {
     }
   }
   alert(szoveg);
+}
+
+function szamol() {
+  var osszeg = 0;
+  for (let i = 0; i < veletlenTomb.length; i++) {
+    osszeg += veletlenTomb[i];
+  }
+  alert("A számok összege: " + osszeg);
+}
+
+function parosak() {
+  var paros = 0;
+  for (let i = 0; i < veletlenTomb.length; i++) {
+    if (veletlenTomb[i] % 2 == 0) {
+      paros++;
+    }
+  }
+  alert("Párosak a száma: " + paros);
+}
+
+function egyes() {
+  var egyes = false;
+  for (let i = 0; i < veletlenTomb.length; i++) {
+    if (veletlenTomb[i] == 1) {
+      egyes = true;
+    }
+  }
+  if (egyes == true) {
+    alert("Van a számok közt egyes.");
+  } else {
+    alert("Nincs egyes a számok közt.");
+  }
+}
+
+function legnagyobb() {
+  var legnagyobbHely = 0;
+  for (let i = 1; i < veletlenTomb.length; i++) {
+    if (veletlenTomb[i] > veletlenTomb[legnagyobbHely]) {
+      legnagyobbHely = i;
+    }
+  }
+  alert("A legnagyobb elem: " + veletlenTomb[legnagyobbHely]);
 }
